8d37deb1b9e491e4fc138dbde4748cc3781270fc
Saket <93667088+SaketChourasiya@users.noreply.github.com>
2024-05-31 14:37:13 +0530

    Update settings.py

--- teckblog/settings.py
+++ teckblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d65#8@-=%$qt(tuh+$w21mpcz)k8hl27opv2#lktxzxj*8ut65
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
